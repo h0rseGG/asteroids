02dc367d9367985bd090194fb9265e8dceeb66fa
h0rseGG <horstingjake@gmail.com>
2025-02-17 14:02:11 +0800

    Asteroid field built with no collision

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import pygame
 from constants import *
+from asteroidfield import *
+from player import Player
 
 def main():
 	
-	pygame.init
+	pygame.init()
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+	
+	clock = pygame.time.Clock()
+	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	Asteroid.containers = (asteroids, updatable, drawable)
+	Player.containers = (updatable, drawable)
+	AsteroidField.containers = (updatable,)
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)	
+	asteroid_field = AsteroidField()
+
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		pygame.Surface.fill(screen, (0,0,0))
+		updatable.update(dt)
+		for object in drawable:
+			object.draw(screen)
 		pygame.display.flip()
+		t = clock.tick(60)
+		dt = t/1000
 
-
-
+	
 
 if __name__ == "__main__":
     main()
